4166d80c36dd98218c679e596809a4b50d2448d7
kdk15 <44698665+kdk15@users.noreply.github.com>
2020-11-20 01:02:42 +0800

    Update home.blade.php

--- resources/views/layouts/partials/home.blade.php
+++ resources/views/layouts/partials/home.blade.php
@@ -2,37 +2,31 @@
 
 @section('content')
 <div class="row">
-    <div class="col-5">
-        <div class="row d-flex justify-content-center py-4">
-            <div class="col-md-8">
-                <h1 class="text-center display-5">Countries</h1>
-            </div>
+    <nav class="col-3 d-none d-block bg-light sidebar">
+        <div class = "sidebar-sticky" style="height:100%; position:fixed; width: 20%; overflow:scroll;">
+            
+                <ul class="nav flex-column">
+                <a class="disabled list-group-item list-group-item-action d-flex justify-content-between align-items-center"><h3>Countries</h3></a>
+            @foreach ($countries as $key => $value)
+                <a href="/{{ $key }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                    {{ $key }}
+                    <span class="badge badge-primary badge-pill">
+                        {{ $value }}
+                    </span>
+                </a>
+            @endforeach
+                </ul>
+            
         </div>
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-8 mb-5">
-                <div class="wrapper shadow rounded pb-5" style="height:80vh; overflow:scroll;">
-                    <ul class="list-group">
-                        @foreach ($countries as $key => $value)
-                        <a href="/{{ $key }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                            {{ $key }}
-                            <span class="badge badge-primary badge-pill">
-                                {{ $value }}
-                            </span>
-                        </a>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-7">
+    </nav>
+    <div class="col-9">
         <div class="row d-flex flex-column py-4">
             <div class="row py-2 d-flex justify-content-between mr-5">
                 <h1>Import / Export</h1>
             </div>
             <div class="row py-2">
                 <div class="chart-wrapper shadow p-5">
-                    <canvas id="importChart" width="350" height="350"></canvas>
+                    <canvas id="importChart" width="550" height="350"></canvas>
                 </div>
                 <div class="col mr-5">
                     <table class="table table-bordered">
@@ -57,7 +51,7 @@
             </div>
             <div class="row py-2">
                 <div class="chart-wrapper shadow p-5">
-                    <canvas id="maxExportChart" width="350" height="350"></canvas>
+                    <canvas id="maxExportChart" width="550" height="350"></canvas>
                 </div>
                 <div class="col mr-5">
                     <table class="table table-bordered">
@@ -80,7 +74,7 @@
             </div>
             <div class="row py-2">
                 <div class="chart-wrapper shadow p-5">
-                    <canvas id="maxImportChart" width="350" height="350"></canvas>
+                    <canvas id="maxImportChart" width="550" height="350"></canvas>
                 </div>
                 <div class="col mr-5">
                     <table class="table table-bordered">
