cec2c0a6a725da07e9b0955705dd741ab2ab9d57
Jason <jasonling1995@gmail.com>
2020-11-20 13:14:46 +0800

    contact update

--- resources/views/layouts/partials/contact.blade.php
+++ resources/views/layouts/partials/contact.blade.php
@@ -12,8 +12,8 @@
                     <img class="card-img-top" src="/img/jason.jpg" alt="Card image cap" style="height: 20vw; width: 100%; object-fit:cover;">
                     <div class="card-body">
                         <h5 class="card-title">Jason Ling</h5>
-                        <p class="card-text">MSc Data Science student from Universiti Teknologi Malaysia</p>
-                        <a href="https://www.linkedin.com/in/jasonling1995/" class="btn btn-primary">LinkedIn</a>
+                        <p class="card-text">MSc Data Science graduate with an interest in Machine Learning, Cloud Computing and Artifial Intelligence</p>
+                        <a href="https://www.linkedin.com/in/jasonling1995/" class="btn btn-primary shadow" target="_">LinkedIn</a>
                     </div>
                 </div>
                 <div class="card shadow" style="width: 18rem;">
@@ -21,7 +21,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Wan Ahmad Luqman</h5>
                         <p class="card-text">MSc Data Science student from Universiti Teknologi Malaysia</p>
-                        <a href="#" class="btn btn-primary">LinkedIn</a>
+                        <a href="#" class="btn btn-primary shadow" target="_">LinkedIn</a>
                     </div>
                 </div>
                 <div class="card shadow" style="width: 18rem;">
@@ -29,7 +29,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Nurnadia binti Khairul Anuar</h5>
                         <p class="card-text">MSc Data Science student from Universiti Teknologi Malaysia</p>
-                        <a href="#" class="btn btn-primary">LinkedIn</a>
+                        <a href="#" class="btn btn-primary shadow" target="_">LinkedIn</a>
                     </div>
                 </div>
             </div>
