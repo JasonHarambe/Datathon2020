fcc19fe62f30bb4b4e186d7b778ac3e1a3f79d69
kdk15 <44698665+kdk15@users.noreply.github.com>
2020-11-26 14:38:07 +0800

    Update ChartController.php

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -72,4 +72,47 @@ class ChartController extends Controller
         return $data;
 
     }
+    
+    public function map()
+    {
+
+        $sum = DB::table('trades')
+        ->select('YEAR', DB::raw('SUM(IMPORT) as IMPORT'), DB::raw('SUM(EXPORT) as EXPORT'))
+        ->groupBy('YEAR')
+        ->get();
+
+        $counts = DB::table('trades')
+        ->select('COUNTRY', DB::raw('count(*) as TOTAL'))
+        ->groupBy('COUNTRY')
+        ->get();
+
+        $ten = DB::table('trades')
+        ->select('COUNTRY', DB::raw('ROUND(SUM(EXPORT) / 1000000, 1) as EXPORT'))
+        ->groupBy('COUNTRY')
+        ->orderBy('EXPORT', 'DESC')
+        ->take(10)
+        ->get();
+
+        $eleven = DB::table('trades')
+        ->select('COUNTRY', DB::raw('ROUND(SUM(IMPORT) / 1000000, 1) as IMPORT'))
+        ->groupBy('COUNTRY')
+        ->orderBy('IMPORT', 'DESC')
+        ->take(10)
+        ->get();
+
+        $countries = $counts->pluck('TOTAL', 'COUNTRY')->toArray();
+
+        $overall = $sum->map(function($sum) {
+            return array(
+                'YEAR' => $sum->YEAR,
+                'IMPORT' => number_format($sum->IMPORT / 1000000),
+                'EXPORT' => number_format($sum->EXPORT / 1000000),
+            );
+        });
+
+        $years = $overall->pluck('YEAR');
+        $imports = $sum->pluck('IMPORT');
+        $exports = $sum->pluck('EXPORT');
+
+        return view('layouts.map', compact('years', 'imports', 'exports', 'countries', 'overall', 'ten', 'eleven'));
 }
