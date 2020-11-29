34721edc6513ca69a4c649131ac821c92ef911e9
kdk15 <44698665+kdk15@users.noreply.github.com>
2020-11-29 18:51:45 +0800

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @yield('head')
@@ -19,15 +19,19 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-            <a class="nav-item nav-link" href="/">Home</a>
-            <a class="nav-item nav-link" href="/interactive">Interactive</a>
-            <a class="nav-item nav-link" href="/infographic">Infographic</a>
-            <a class="nav-item nav-link" href="/map">Map</a>
-            <a class="nav-item nav-link" href="/about">About</a>
-            <a class="nav-item nav-link" href="/contact">Contact</a>
-            </div>
+        <div class="collapse navbar-collapse" id="navbarfirst">
+            <ul class="navbar-nav">
+            <li class="nav-item nav-link" href="/">Home</li>
+            <li class="nav-item nav-link" href="/interactive">Interactive</li>
+            <li class="nav-item nav-link" href="/infographic">Infographic</li>
+            <li class="nav-item nav-link" href="/map">Map</li>
+            <ul>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item nav-link" href="/about">About</li>
+                <li class="nav-item nav-link" href="/contact">Contact</li>
+            </ul>
         </div>
     </nav>
     @yield('content')
